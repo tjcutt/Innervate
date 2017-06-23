47c633a7092bed5594af27328fe8d2a138d4d58b
Lisa Ma <limawebdev@gmail.com>
2017-06-23 14:01:09 -0600

    made home page components and linked up

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import { Link } from 'react-router-dom'
 import './App.css';
 import HomeNav from './components/HomeNav'
+import HomeInfo from './components/HomeInfo'
+import HomeForm from './components/HomeForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HomeNav />
+        <HomeInfo />
+        <HomeForm />
         <Link to="/proposals"> PROProsals </Link>
       </div>
     );
