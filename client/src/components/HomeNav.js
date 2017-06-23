e6ca72cdc629116431c6a53a5248fa3936f53eb8
Tim <tjcutt@gmail.com>
2017-06-23 16:12:51 -0600

    homeNav components added

--- client/src/components/HomeNav.js
+++ client/src/components/HomeNav.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
+// import AppBar from 'material-ui/AppBar';
+// import TextField from 'material-ui/TextField';
 
 class HomeNav extends React.Component{
   constructor(props){
@@ -10,9 +11,28 @@ class HomeNav extends React.Component{
   }
   render() {
     return (
-       <MuiThemeProvider>
-        <AppBar title="Title"/>
-      </MuiThemeProvider>
+       <div>
+        <nav>
+            <div className="nav-wrapper">
+              <h1 className="brand-logo ">Logo</h1>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li className="input-field col s6">
+                 <input placeholder="email" id="first_name" type="text" className="validate"/>
+                 <label htmlFor="first_name">First Name</label>
+               </li>
+
+                <li className="input-field col s6">
+                 <input placeholder="password" id="first_name" type="text" className="validate"/>
+                 <label htmlFor="first_name">First Name</label></li>
+
+                 <li> <a className="waves-effect waves-light btn">Login</a>
+</li>
+              </ul>
+            </div>
+          </nav>
+
+
+        </div>
     );
   }
 }
