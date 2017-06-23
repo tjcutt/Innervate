5ef9439fe04f4478bbc7df55b2e6d8b0fb7539dd
lerning <joshuabenlerner@gmail.com>
2017-06-23 13:28:29 -0600

    proposals component made, materialize has begun

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Proposals from './components/Proposals.js'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import {BrowserRouter, Route} from 'react-router-dom';
@@ -9,13 +10,10 @@ ReactDOM.render(
   <BrowserRouter>
     <div className="App">
       <Route exact path="/" component={App}></Route>
+      <Route path="/proposals" component={Proposals}></Route>
     </div>
 
-
   </BrowserRouter>,
-
-
-
 
   document.getElementById('root')
 
