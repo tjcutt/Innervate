8e8e05761ed29fab22841712268e56354e796104
lerning <joshuabenlerner@gmail.com>
2017-06-23 15:24:01 -0600

    material UI functional, dropdown select is now working

---
+++ client/src/components/MultiSelect.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+const names = [
+  'Lemon',
+  'Give',
+  'Me',
+  'A',
+  'Highfive',
+];
+
+/**
+ * `SelectField` can handle multiple selections. It is enabled with the `multiple` property.
+ */
+export default class SelectFieldExampleMultiSelect extends Component {
+  state = {
+    values: [],
+  };
+
+  handleChange = (event, index, values) => this.setState({values});
+
+  menuItems(values) {
+    return names.map((name) => (
+      <MenuItem
+        key={name}
+        insetChildren={true}
+        checked={values && values.indexOf(name) > -1}
+        value={name}
+        primaryText={name}
+      />
+    ));
+  }
+
+  render() {
+    const {values} = this.state;
+    return (
+      <SelectField
+        multiple={true}
+        hintText="Select a name"
+        value={values}
+        onChange={this.handleChange}
+      >
+        {this.menuItems(values)}
+      </SelectField>
+    );
+  }
+}
--- client/src/components/Proposals.js
+++ client/src/components/Proposals.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+import MultiSelect from "./MultiSelect"
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Proposals extends React.Component {
 
    render(){
       return (
-      <div>
-      </div>
+
+      <MuiThemeProvider>
+         <div className="container">
+            <h1 id="timbo"> Proposals!!</h1>
+            <MultiSelect />
+            <RaisedButton label="New +" primary={true} />
+         </div>
+      </MuiThemeProvider>
+
       )
    }
 
