20061f3b2704e9cb894e3fc5d43a5e67da35d2c6
lerning <joshuabenlerner@gmail.com>
2017-06-26 13:38:01 -0600

    beginning css work on proposals page, in conjunction with sort/filter funcionality

--- client/src/components/Proposals.js
+++ client/src/components/Proposals.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import {Container, Row, Col} from 'react-grid-system'
+// import {Input, Row, Col} from 'react-grid-system'
+import {Input} from 'react-materialize'
 import MultiSelect from "./MultiSelect"
 import ProposalList from './ProposalList'
 
@@ -35,14 +36,18 @@ class Proposals extends React.Component {
                <button className="btn newBtn">I am gr8</button></h1>
             </div>
             <div className="row">
-               <MultiSelect  className="col s2 m2" />
                <div className="input-field col s4 m4 l6">
                 <input id="filter" type="text" className="validate" />
                 <label htmlFor="filter">Filter</label>
                </div>
+               <Input s={4} type='select' label="Sort" defaultValue=''>
+            		<option value='popular'>Popular</option>
+            		<option value='new'>New</option>
+            		<option value='something'>Something</option>
+            	</Input>
             </div>
          </div>
-         <div>
+         <div className="container">
             <ProposalList proposals={this.state.proposals}/>
          </div>
       </div>
