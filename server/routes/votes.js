b590e1c589efbd7ef8e7cbd7bd11b8cb63dd124c
lerning <joshuabenlerner@gmail.com>
2017-06-27 16:37:12 -0600

    voting system functional

--- server/routes/votes.js
+++ server/routes/votes.js
@@ -2,22 +2,38 @@ var express = require('express');
 var router = express.Router();
 const knex = require('../knex');
 
+router.post('/:id', (req, res, next) => {
+   console.log('backend id', req.body);
+   let proposalId = req.body.id
+   knex('votes')
+      .where('proposal_id', proposalId)
+      .count('active')
+      .then((num)=> {
+         console.log('nummy', num[0].count);
+         res.send(num[0].count)
+      })
+})
+
 router.post('/', function(req, res, next) {
-   console.log('reqbody', req.body);
    let userId = req.body.userId
    let proposalId = req.body.proposalId
+   let body =
+        { 'proposal_id': proposalId,
+          'user_id': 1,
+          'active': true }
    knex('votes')
    .where("user_id", userId)
    .where("proposal_id", proposalId)
    .where("active", true)
    .then((data) => {
-      console.log('then res',data.length);
       if (data.length > 0) res.json(data)
       else {
          knex('votes')
-            .insert( )
+            .insert(body)
+            .then ((data) => {
+               res.json(data)
+            })
       }
-      res.json(data);
    })
 });
 
