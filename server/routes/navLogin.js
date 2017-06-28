7e4f79de1651a4524c228f2f46b84d96563527e5
Tim <tjcutt@gmail.com>
2017-06-28 11:20:27 -0600

    bcryptcompare implemented, but bugs need to be worked on

--- server/routes/navLogin.js
+++ server/routes/navLogin.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 require('dotenv')
 
 router.post('/', function(req, res, next) {
@@ -12,12 +13,13 @@ router.post('/', function(req, res, next) {
     .where('email', req.body.email)
     .select('*')
     .then((data) => {
-      // console.log('data[0]', data[0], req.body.email)
+      console.log(data[0].hashed_pass);
+      console.log(req.body.hashed_pass);
+      if (bcrypt.compareSync(data[0].hashed_pass, req.body.hashed_pass)) {
+        console.log('It works!!!');
+      }
+      else {console.log('what are you doing?!!')}
        let tokens = setTokens(data[0])
-      // return tokens
-      // res.setCookie('hey', 'tim')
-      // res.cookie('user', tokens[0], {httpOnly: true})
-      // res.cookie('role', tokens[1], {httpOnly: true})
       res.send(tokens);
     })
     // .then(res => res.json({tokens}))
