23589f610a6755da265b42727839c654ca1865c9
Lisa Ma <limawebdev@gmail.com>
2017-07-12 13:58:10 -0600

    fixed sextuply nested knex call in survey

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ if (process.env.NODE_ENV !== `production`) {
   require(`dotenv`).config();
 }
 
-
 const express = require(`express`);
 const path = require(`path`);
 const logger = require(`morgan`);
--- server/routes/survey.js
+++ server/routes/survey.js
@@ -4,57 +4,72 @@ const knex = require('../knex');
 const jwt = require('jsonwebtoken');
 
 router.get('/', function(req, res, next) {
-  res.send('i connect');
+ res.send('i connect');
 });
 
-router.post('/', function(req, res, next){
-  let userInfo = jwt.verify(req.body.userCookie, process.env.JWT_SECRET)
-  let userCookie = userInfo.user
-  if(req.body.referral == 'otherRef'){
-    req.body.referral = 'Other'
-  }
-  knex('referrals')
-    .where('name', req.body.referral)
-    .then((referral) => {
-      knex('users')
-        .where('id', userCookie.id)
-        .returning('*')
-        .update({
-          'referral_id': referral[0].id
-        })
-        .then((user) => {
-          knex('disorders')
-            .select('*')
-            .where('name', req.body.disorders[0])
-            .then((disorders) => {
-                knex('user_disorder')
-                  .returning('*')
-                  .insert({
-                    disorder_id:disorders[0].id,
-                    user_id:user[0].id
-                  })
-                  .then((user_disorder)=>{
-                      knex('roles')
-                        .where('name', req.body.role)
-                        .then((role) => {
-                          knex('user_role')
-                            .returning('*')
-                            .insert({
-                              user_id:user[0].id,
-                              role_id:role[0].id
-                            })
-                            .then((data) => {
-                              let roleData = data[0].role_id
-                              let role = jwt.sign({
-                                  role: roleData
-                              }, process.env.JWT_SECRET)
-                              res.send({role:role});
-                            })
-                        })
-                  })
-            })
-        })
+router.post('/', function(req, res, next) {
+ let userInfo = jwt.verify(req.body.userCookie, process.env.JWT_SECRET)
+ let userCookie = userInfo.user
+ if (req.body.referral == 'otherRef') {
+  req.body.referral = 'Other'
+ }
+ knex('referrals')
+  .where('name', req.body.referral)
+  .then((referral) => {
+   knex('users')
+    .where('id', userCookie.id)
+    .returning('*')
+    .update({
+     'referral_id': referral[0].id
     })
+    .then((user) => {
+      return insertDisorders(req.body.disorders, userCookie.id)
+    })
+    .then((thing) => {
+      return insertUserRole(req.body.role, userCookie.id)
+    })
+    .then((roleInsert) => {
+      let roleData = roleInsert.role_id
+      let role = jwt.sign({
+       role: roleData
+      }, process.env.JWT_SECRET)
+      res.send({
+       role: role
+      });
+    })
+  })
 })
 
+function insertUserRole(reqRole, userID){
+  return knex('roles')
+    .where('name', reqRole)
+    .then(role => {
+      return knex('user_role')
+        .returning('*')
+        .insert({
+          user_id:userID,
+          role_id:role[0].id
+        })
+        .then((data) => {
+          return data[0];
+        })
+    })
+}
+
+function insertDisorders(disorders, userID) {
+ return knex('disorders')
+  .whereIn('name', disorders)
+  .then((disorders) => {
+   return Promise.all(
+    disorders.map(function(disorder) {
+     return knex('user_disorder')
+      .returning('*')
+      .insert({
+       disorder_id: disorder.id,
+       user_id: userID
+      })
+    })
+   )
+  })
+}
 module.exports = router;
